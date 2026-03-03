6263b015a3fcc560a183b8c0ce6eb34711b17a57
sameer-a12 <sameerali06567@gmail.com>
2026-03-03 11:48:54 +0530

    setup configured

--- src/app.js
+++ src/app.js
@@ -1,7 +1,23 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
 
 const app = express()
 
-// ... your middlewares (cors, cookieParser, etc.) ...
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
 
-export { app } // This is the line you are likely missing!
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+
+import userRouter from "./routes/user.routes.js";
+
+app.use("/api/v1/users", userRouter);
+
+
+export {app}
